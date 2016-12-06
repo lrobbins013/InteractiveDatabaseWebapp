1293ea8de69e90e0de280018e0fd3decf7125b07
hilll <levihill@Levis-MacBook-Air.local>
2016-12-05 19:14:25 -0700

    Updating the files in gitHub

--- Payment.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE levihill.Payment;
-
-create table levihill.Payment
-( patID number(4) NOT NULL,
-billingInfo varchar2(30) NOT NULL,
-cardNum number(4) NOT NULL );
-
-GRANT SELECT ON levihill.Payment TO PUBLIC;
-
